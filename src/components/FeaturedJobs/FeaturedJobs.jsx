405130ae5b76500bda4c2779de5319ef3abbdca1
rimiruma <rimiruma12@.com>
2023-09-29 15:54:31 +0600

    toast apply

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,7 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+const [dataLength, setDataLength] = useState(4);
 
 useEffect(() =>{
     fetch('jobs.json')
@@ -17,10 +18,15 @@ useEffect(() =>{
             <h2>Featured Jobs: {jobs.length}</h2>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div className=" mx-auto grid grid-cols-3 gap-6">
+            <div className=" mx-auto grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs. slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={ dataLength === jobs.length ? 'hidden' : ''}>
+                <button 
+                onClick={() => setDataLength(jobs.length)}
+                className="btn btn-primary">Show All Jobs</button>
             </div>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import Root from './components/Root/Root';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 import Home from './components/Home/Home';
 import AppliedJobs from './components/AppliedJobs/AppliedJobs';
-import ErrorPage from './components/ErrorPage/ErrorPage';
+import JobDetails from './components/JobDetails/JobDetails';
+
 
 const router = createBrowserRouter([
   {
@@ -23,7 +22,11 @@ const router = createBrowserRouter([
       {
         path: "/applied",
         element: <AppliedJobs></AppliedJobs>
-
+      },
+      {
+        path: '/job/:id',
+        element: <JobDetails></JobDetails>,
+        loader: () => fetch('../jobs.json')
       }
     ]
   },
